173ead06c361186ec3163c5814fc085f3174d49c
Recon15698 <40797621+Recon15698@users.noreply.github.com>
2021-02-17 21:47:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors")
-const path = require("path");
 
 const app = express();
 
@@ -60,14 +59,7 @@ app.post("/api/insert",function(req,res){
 
 
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-  }
+
 
 
 let port = process.env.PORT;
